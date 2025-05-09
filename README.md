30204f971c1cff1a64ee603f91ce53324700ee52
MagnusPC <103988876+MagnusPC@users.noreply.github.com>
2025-05-09 13:25:55 +0200

    eod

---
+++ README.md
@@ -0,0 +1,6 @@
+# readme
+a random string generator written in java for time and memory effeciency https://thenewstack.io/which-programming-languages-use-the-least-electricity/
+
+end goal is to have a function for creating a set of randomly generated strings based on a regex, a string length and a set count
+
+ideally i would test both stringbuilder versus a random number generator with hex values to create the strings to see which works best
--- src/string_generator/String_generator.java
+++ src/string_generator/String_generator.java
@@ -1,9 +1,11 @@
 package string_generator;
 
 import java.util.ArrayList;
-import java.util.regex.*;
-import java.util.stream.IntStream;
+import java.util.HexFormat; //Use to convert decimal to hex, and from hex to char unicode values
+import java.util.regex.*; //bounding of requested strings
+import java.util.stream.IntStream; //rnd number stream of requested size
 import java.util.random.*;
+import java.lang.StringBuilder; //Alt. tool to generate strings
 
 public class String_generator {
 
@@ -18,16 +20,17 @@ public class String_generator {
 		System.out.println("Job done");
 	}
 	
-	public String[] Generator() {
+	protected void GeneratorWithUnicode() {
 		
 		//local regex pattern to use when generating the strings
-		Pattern regexToUse = Pattern.compile("[a-zA-Z][0-9]"/*String pattern, int flags*/);
+		Pattern regexToUse = Pattern.compile("[a-zA-Z][0-9]"); //params: string pattern, int flags (binary mask)
 		
 		//length of requested strings
 		int lengthOfString = 10;
 		
-		//initialize the randoom generator
+		//initialize the random generator
 		IntStream rng = RandomGenerator.getDefault().ints(lengthOfString); //creates a stream of count/size/length 10
+		convertRandomNumberToHex(rng);
 		
 		//list to keep track of generated strings, and to return all strings as output, TODO make unique value
 		ArrayList<String> listOfStrings = new ArrayList<String>();
@@ -36,6 +39,26 @@ public class String_generator {
 			System.out.println(rng.toArray()[i]);
 		}
 		
-		return null;
 	}
+	
+	protected void GeneratorWithStringBuilder() {
+		int lengthOfString = 10;
+		StringBuilder strBuild = new StringBuilder(lengthOfString);
+		
+		int rnd = (int)(Math.random() * 100); //TODO lav en metode der opretter et arr ud fra x length, hvert index skal være et random int mellem 48 og 122 jf. unicode
+		
+		for (int i = 0; i < lengthOfString; i++) {
+//			strBuild. //append a char corresponding to the value at index i in the intstream
+			
+		}
+		
+	}
+	
+	protected HexFormat convertRandomNumberToHex(IntStream numbers) {
+//		numbers.
+		return null;
+		
+	}
+	
+	
 }
