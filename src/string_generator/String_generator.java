ed58249cd2e4c2f76cc816e48c0ffba24a0dfdd5
MagnusPC <103988876+MagnusPC@users.noreply.github.com>
2025-05-09 09:55:29 +0200

    init fra git cli

---
+++ src/string_generator/String_generator.java
@@ -0,0 +1,20 @@
+package string_generator;
+
+public class String_generator {
+
+	//TODO make interface
+	//TODO make accessor method
+	//TODO make random string generator method
+		//TODO refactor to take regex and length as input parameter
+		//TODO connect to redis db? TODO create strings as dict (or other unique value alternatives)
+	
+	//TODO dont run from the generator-file itself
+	public static void main(String[] args) {
+		System.out.println("Hello rndgen");
+	}
+	
+	public String[] Generator() {
+		
+		return null;
+	}
+}
